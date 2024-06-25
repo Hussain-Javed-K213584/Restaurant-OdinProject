3b58b54dc83a5d80454d6d79cc7b5f63d733294b
Muhammad Hussain <maango@localhost.localdomain>
2024-06-25 11:23:20 +0500

    Added dist folder

---
+++ home.bundle.js
@@ -0,0 +1,66 @@
+/*
+ * ATTENTION: The "eval" devtool has been used (maybe by default in mode: "development").
+ * This devtool is neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
+/******/ (() => { // webpackBootstrap
+/******/ 	"use strict";
+/******/ 	var __webpack_modules__ = ({
+
+/***/ "./src/home.js":
+/*!*********************!*\
+  !*** ./src/home.js ***!
+  \*********************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpack_require__.d(__webpack_exports__, {\n/* harmony export */   addHomeHeading: () => (/* binding */ addHomeHeading),\n/* harmony export */   addHomeText: () => (/* binding */ addHomeText)\n/* harmony export */ });\nfunction addHomeHeading(){\n    // Create a heading with underline, make it bold as well\n    // Return the node\n\n    const homeHeading = document.createElement('h2');\n    homeHeading.innerText = 'A Random Restaurant';\n    homeHeading.style.textAlign = 'center';\n    homeHeading.style.textDecoration = 'underline';\n    homeHeading.style.fontWeight = '600';\n    homeHeading.style.color = 'red';\n\n\n    return homeHeading;\n}\n\nfunction addHomeText(){\n    // This function will add some home text\n    const welcomeParagraph = document.createElement('p');\n    welcomeParagraph.innerText = `  Welcome to our restaurant. \n                                    You can start by viewing our Menu or you may order online through call.\n                                    Our number can be seen from the About page.\n                                    `;\n    welcomeParagraph.style.color = 'lightblue';\n    welcomeParagraph.style.textAlign = 'center';\n\n    return welcomeParagraph;\n    \n}\n\n\n//# sourceURL=webpack://restaurant-odinproject/./src/home.js?");
+
+/***/ })
+
+/******/ 	});
+/************************************************************************/
+/******/ 	// The require scope
+/******/ 	var __webpack_require__ = {};
+/******/ 	
+/************************************************************************/
+/******/ 	/* webpack/runtime/define property getters */
+/******/ 	(() => {
+/******/ 		// define getter functions for harmony exports
+/******/ 		__webpack_require__.d = (exports, definition) => {
+/******/ 			for(var key in definition) {
+/******/ 				if(__webpack_require__.o(definition, key) && !__webpack_require__.o(exports, key)) {
+/******/ 					Object.defineProperty(exports, key, { enumerable: true, get: definition[key] });
+/******/ 				}
+/******/ 			}
+/******/ 		};
+/******/ 	})();
+/******/ 	
+/******/ 	/* webpack/runtime/hasOwnProperty shorthand */
+/******/ 	(() => {
+/******/ 		__webpack_require__.o = (obj, prop) => (Object.prototype.hasOwnProperty.call(obj, prop))
+/******/ 	})();
+/******/ 	
+/******/ 	/* webpack/runtime/make namespace object */
+/******/ 	(() => {
+/******/ 		// define __esModule on exports
+/******/ 		__webpack_require__.r = (exports) => {
+/******/ 			if(typeof Symbol !== 'undefined' && Symbol.toStringTag) {
+/******/ 				Object.defineProperty(exports, Symbol.toStringTag, { value: 'Module' });
+/******/ 			}
+/******/ 			Object.defineProperty(exports, '__esModule', { value: true });
+/******/ 		};
+/******/ 	})();
+/******/ 	
+/************************************************************************/
+/******/ 	
+/******/ 	// startup
+/******/ 	// Load entry module and return exports
+/******/ 	// This entry module can't be inlined because the eval devtool is used.
+/******/ 	var __webpack_exports__ = {};
+/******/ 	__webpack_modules__["./src/home.js"](0, __webpack_exports__, __webpack_require__);
+/******/ 	
+/******/ })()
+;
